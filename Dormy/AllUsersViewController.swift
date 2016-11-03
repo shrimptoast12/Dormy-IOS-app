15a704892efec89ce4a358d507bc58549590b375
lee-benjamin <leebenjamin@utexas.edu>
2016-11-03 11:57:46 -0500

    Added a cache for images... laggy loading (sometimes with the wrong image to the wrong name) only visible on the first time you try to load. afterwards, images will be cached and be mapped to the right user

--- Dormy/AllUsersViewController.swift
+++ Dormy/AllUsersViewController.swift
@@ -53,21 +53,48 @@ class AllUsersViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("cellId", forIndexPath: indexPath) as! UserCell
         
         let user = users[indexPath.row]
-        
         cell.textLabel?.text = user.name
+        
         if let profileImageUrl = user.imageURL {
-            let url = NSURL(string: profileImageUrl)
-            NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) in
-                if error != nil {
-                    print(error)
-                    return
-                }
-                dispatch_async(dispatch_get_main_queue(), {
-                    cell.profileImageView.image = UIImage(data: data!)
-                })
- 
-            }).resume()
+            cell.profileImageView.loadImageUsingCacheWithUrlString(profileImageUrl)
         }
         return cell
+    }
+}
+
+let imageCache = NSCache()
+    
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(urlString: String) {
+        
+        self.image = nil
+        
+        //check cache for image first
+        if let cachedImage = imageCache.objectForKey(urlString) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        //otherwise fire off a new download
+        let url = NSURL(string: urlString)
+        NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) in
+            
+            //download hit an error so lets return out
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString)
+                    
+                    self.image = downloadedImage
+                }
+            })
+            
+        }).resume()
     }
 }
